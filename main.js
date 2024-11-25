a1a823c153aaae5567e8a174f5748bba0b9a4c3f
iniadittt <aditya.braincore@gmail.com>
2024-11-25 17:35:32 +0700

    UPDATE

---
+++ main.js
@@ -0,0 +1,46 @@
+const schedule = require("node-schedule");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+
+const time = 4 * 1000;
+// const time = 10 * 60 * 1000;
+
+async function CHANGE() {
+	const readmePath = path.join(__dirname, "README.md");
+	const README = fs.readFileSync(readmePath, "utf8");
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = String(today.getMonth() + 1).padStart(2, "0");
+	const date = String(today.getDate()).padStart(2, "0");
+	const hours = String(today.getHours()).padStart(2, "0");
+	const minutes = String(today.getMinutes()).padStart(2, "0");
+	const seconds = String(today.getSeconds()).padStart(2, "0");
+	const formattedDate = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+	const NEWREADME = README.split("---")
+		.map((x, i) => (i === 1 ? ` Waktu saat ini: ${formattedDate} WIB ` : x))
+		.join("---");
+	fs.writeFileSync(readmePath, NEWREADME);
+}
+
+async function executeBatScript() {
+	const batPath = path.resolve(__dirname, "gitpush.bat");
+	exec(batPath, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error saat menjalankan .bat: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`Error output: ${stderr}`);
+			return;
+		}
+		console.log(`Output dari script: ${stdout}`);
+	});
+}
+
+setInterval(() => {
+	CHANGE();
+	executeBatScript();
+}, time);
+
+console.log("Job telah dijadwalkan.");
