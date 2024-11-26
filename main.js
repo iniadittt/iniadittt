ef42479d5a410d5be07b7f043b48c97cba0d969d
iniadittt <aditya.braincore@gmail.com>
2024-11-26 09:16:48 +0700

    UPDATE

--- main.js
+++ main.js
@@ -7,18 +7,23 @@ const time = 3 * 1 * 1000;
 
 async function CHANGE() {
 	const readmePath = path.join(__dirname, "README.md");
+	console.log({ readmePath });
 	const README = fs.readFileSync(readmePath, "utf8");
+	console.log({ README });
 	const today = new Date();
+	console.log({ today });
 	const year = today.getFullYear();
 	const month = String(today.getMonth() + 1).padStart(2, "0");
 	const date = String(today.getDate()).padStart(2, "0");
 	const hours = String(today.getHours()).padStart(2, "0");
 	const minutes = String(today.getMinutes()).padStart(2, "0");
-	const seconds = String(today.getSeconds()).padStar(2, "0");
+	const seconds = String(today.getSeconds()).padStart(2, "0");
 	const formattedDate = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+	console.log({ formattedDate });
 	const NEWREADME = README.split("---")
 		.map((x, i) => (i === 1 ? ` Last update: ${formattedDate} WIB ` : x))
 		.join("---");
+	console.log({ NEWREADME });
 	fs.writeFileSync(readmePath, NEWREADME);
 	return;
 }
